5d39094dd7ff993cb14b4fe0ff2bde2a3495c993
sanyamjindal <101120349+sanyamjindal@users.noreply.github.com>
2023-09-23 19:39:54 +0530

    Push

--- day1.cpp
+++ day1.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
+
+    long n;
+    long rev = 0;
+    long temp;
+    cout<<"Enter The Integer:";
+    cin>>n;
+
+    while (n!=0)
+    {
+    temp = n % 10;
+    rev = rev * 10 + temp ;
+    n = n / 10;
+    }
     
-    cout<<"Hello World";
+    cout<<rev;
     return 0;
 }
